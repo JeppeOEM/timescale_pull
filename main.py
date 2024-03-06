09ce977f61e770a7ac329953d570231caa186c1f
Jeppe <jepp026b@edu.zealand.dk>
2024-03-06 20:56:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,91 @@
+from decouple import config
+from binance import ThreadedWebsocketManager
+import psycopg2
+import datetime
+import time
+
+api_key = config("API_KEY")
+api_secret = config("API_SECRET")
+db_pass = config("DB")
+
+def main():
+    symbol = "ETHUSDT"
+    max_retries = 5
+    retry_delay = 2  # seconds
+
+    for retry_count in range(1, max_retries + 1):
+        try:
+            connection = psycopg2.connect(user="postgres",
+                                          password=config("DB"),
+                                          host="127.0.0.1",
+                                          port="5432",
+                                          database="postgres")
+            cursor = connection.cursor()    
+
+            stream = ["ethusdt@trade", "btcusdt@trade"] #pair@sockettype
+            twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+            twm.start()
+
+            def handle_socket_message(msg, cursor=cursor):
+                msg = msg["data"]
+                print(msg)
+
+                query = "INSERT INTO raw_trade_data (TIME, SYMBOL, PRICE, QUANTITY)"+ \
+                        " VALUES (%s,%s,%s,%s)"
+                timestamp = datetime.datetime.fromtimestamp(int(msg["T"] / 1000))
+                record_to_insert = (timestamp, msg["s"], msg["p"], msg["q"])
+                cursor.execute(query, record_to_insert)
+                print(record_to_insert)
+                connection.commit()
+
+            twm.start_multiplex_socket(callback=handle_socket_message, streams=stream)
+            twm.join()  # block everything after until the websocket is finished
+
+        except (psycopg2.Error, Exception) as error:
+            print(f"Error: {error}")
+            if retry_count < max_retries:
+                print(f"Retrying in {retry_delay} seconds (attempt {retry_count}/{max_retries})...")
+                time.sleep(retry_delay)
+            else:
+                print(f"Max retry attempts reached. Exiting.")
+                break
+        finally:
+            # Close the WebSocket connection
+            twm.stop()
+            twm.join()
+
+            # Close the database connection
+            if connection:
+                connection.close()
+                print("Database connection closed.")
+
+if __name__ == "__main__":
+    main()
+
+#docker exec -it timescaledb psql -U postgres
+
+# CREATE TABLE IF NOT EXISTS raw_trade_data (
+#    time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+#    symbol text NOT NULL,
+#    price double PRECISION NOT NULL,
+#    quantity double PRECISION NOT NULL
+# );
+
+#view table
+#\dt
+#SELECT create_hypertable('raw_trade_data','time') # time is the timestamp columns name    
+#
+#    )
+# {
+#   "e": "trade",     // Event type
+#   "E": 123456789,   // Event time
+#   "s": "BNBBTC",    // Symbol
+#   "t": 12345,       // Trade ID
+#   "p": "0.001",     // Price
+#   "q": "100",       // Quantity
+#   "b": 88,          // Buyer order ID
+#   "a": 50,          // Seller order ID
+#   "T": 123456785,   // Trade time
+#   "m": true,        // Is the buyer the market maker?
+#   "M": true         // Ignore
+# }
---
+++ pull.py
@@ -0,0 +1,67 @@
+from binance.client import Client
+import json
+import pandas as pd
+import websocket #need pip install websocket-client and websocket
+import pprint
+client = Client()
+
+dict_ = client.get_exchange_info()
+
+# [i['symbol'] for i in dict_['symbol']]
+sym = [i['symbol'] for i in dict_['symbols'] if i['symbol'].endswith('USDT')]
+# filter out forex and up down
+print(len(sym))
+
+sym = [i.lower() + '@kline_1m' for i in sym]
+
+stream_these = "/".join(sym)
+
+def manipulate(data):
+    try:
+        value_dict = data['data']['k']
+        price, sym = value_dict['c'], value_dict['s']
+        event_time = pd.to_datetime([data['data']['E']],unit='ms')
+        df = pd.DataFrame([[price,sym]], index=event_time)
+    except Exception as e:
+        print(e) 
+
+    return df
+
+def on_message(ws, message):
+    json_message = json.loads(message)
+    df = manipulate(json_message)
+    df.to_csv('coinprices.csv',mode='a',header=False)
+    print(df)
+
+
+socket = "wss://stream.binance.com:9443/stream?streams="+stream_these
+
+ws=websocket.WebSocketApp(socket, on_message=on_message)
+ws.run_forever()
+
+
+
+# {
+#   "e": "kline",     // Event type
+#   "E": 1672515782136,   // Event time
+#   "s": "BNBBTC",    // Symbol
+#   "k": {
+#     "t": 123400000, // Kline start time
+#     "T": 123460000, // Kline close time
+#     "s": "BNBBTC",  // Symbol
+#     "i": "1m",      // Interval
+#     "f": 100,       // First trade ID
+#     "L": 200,       // Last trade ID
+#     "o": "0.0010",  // Open price
+#     "c": "0.0020",  // Close price
+#     "h": "0.0025",  // High price
+#     "l": "0.0015",  // Low price
+#     "v": "1000",    // Base asset volume
+#     "n": 100,       // Number of trades
+#     "x": false,     // Is this kline closed?
+#     "q": "1.0000",  // Quote asset volume
+#     "V": "500",     // Taker buy base asset volume
+#     "Q": "0.500",   // Taker buy quote asset volume
+#     "B": "123456"   // Ignore
+#   }
+# }
